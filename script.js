0ae025f77b414d418915fb74d96fbedc41fd1ed7
Wmtmky <75718016+Wmtmky@users.noreply.github.com>
2022-03-05 11:05:30 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// setup page on load
+window.onload = function () {
+    setViewport();
+}
+
+// maintain viewport height for soft keyboard on android
+function setViewport () {
+    var viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", viewport.content + ", height = " + window.innerHeight);
+}
+
